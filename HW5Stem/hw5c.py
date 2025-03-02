e89e80f150e1ebda62b5f841eb64416bfba038b5
jkrei00r <jaminkreighbaum@gmail.com>
2025-03-02 10:20:37 -0600

    Update hw5c.py

--- HW5Stem/hw5c.py
+++ HW5Stem/hw5c.py
@@ -1,6 +1,6 @@
 # region imports
 import numpy as np
-from scipy.integrate import #JES MISSING CODE
+from scipy.integrate import solve_ivp  # Import solve_ivp from scipy.integrate
 import matplotlib.pyplot as plt
 # endregion
 
@@ -19,71 +19,67 @@ def ode_system(t, X, *params):
     :param params: the list of physical constants for the system.
     :return: The list of derivatives of the state variables.
     '''
-    #unpack the parameters
-    A, Cd, ps, pa, V, beta, rho, Kvalve, m, y=params
+    # Unpack the parameters
+    A, Cd, ps, pa, V, beta, rho, Kvalve, m, y = params
 
-    #state variables
-    #X[0]=x
-    #X[1]=xdot
-    #X[2]=p1
-    #X[3]=p2
+    # State variables
+    x = X[0]  # Position of the piston
+    xdot = X[1]  # Velocity of the piston
+    p1 = X[2]  # Pressure on the right of the piston
+    p2 = X[3]  # Pressure on the left of the piston
 
-    #calculate derivitives
-    #conveniently rename the state variables
-    x = #JES MISSING CODE
-    xdot = #JES MISSING CODE
-    p1 = #JES MISSING CODE
-    p2 = #JES MISSING CODE
+    # Calculate derivatives
+    xddot = (p1 - p2) * A / m  # Acceleration based on pressure difference
+    p1dot = (y * Kvalve * (ps - p1) - rho * A * xdot) * beta / (V * rho)  # Pressure derivative for p1
+    p2dot = -(y * Kvalve * (p2 - pa) - rho * A * xdot) * beta / (V * rho)  # Pressure derivative for p2
 
-    #use my equations from the assignment
-    xddot = #JES MISSING CODE
-    p1dot = #JES MISSING CODE
-    p2dot = #JES MISSING CODE
-
-    #return the list of derivatives of the state variables
-    return [#JES MISSING CODE]
+    # Return the list of derivatives of the state variables
+    return [xdot, xddot, p1dot, p2dot]
 
 def main():
-    #After some trial and error, I found all the action seems to happen in the first 0.02 seconds
-    t=np.linspace(0,0.02,200)
-    #myargs=(A, Cd, Ps, Pa, V, beta, rho, Kvalve, m, y)
-    myargs=(4.909E-4, 0.6, 1.4E7,1.0E5,1.473E-4,2.0E9,850.0,2.0E-5,30, 0.002)
-    #because the solution calls for x, xdot, p1 and p2, I make these the state variables X[0], X[1], X[2], X[3]
-    #ic=[x=0, xdot=0, p1=pa, p2=pa]
-    pa = #JES MISSING CODE
-    ic = #JES MISSING CODE
-    #call odeint with ode_system as callback
-    sln=solve_ivp(#JES MISSING CODE)
+    # Define the time array, from 0 to 0.02 seconds, with 200 points
+    t = np.linspace(0, 0.02, 200)
 
-    #unpack result into meaningful names
-    xvals=sln.y[0]
-    xdot=sln.y[1]
-    p1=sln.y[2]
-    p2=sln.y[3]
+    # Parameters for the system
+    myargs = (4.909E-4, 0.6, 1.4E7, 1.0E5, 1.473E-4, 2.0E9, 850.0, 2.0E-5, 30, 0.002)
 
-    #plot the result
+    # Initial conditions: x = 0, xdot = 0, p1 = pa, p2 = pa
+    pa = myargs[3]  # pa is the ambient pressure
+    ic = [0, 0, pa, pa]  # Initial conditions
+
+    # Call solve_ivp to solve the system of ODEs
+    sln = solve_ivp(ode_system, [0, 0.02], ic, args=myargs, t_eval=t)
+
+    # Unpack result into meaningful names
+    xvals = sln.y[0]  # Position of the piston
+    xdot = sln.y[1]  # Velocity of the piston
+    p1 = sln.y[2]  # Pressure on the right of the piston
+    p2 = sln.y[3]  # Pressure on the left of the piston
+
+    # Plot the velocity (xdot) as a function of time
     plt.subplot(2, 1, 1)
-    plt.subplot(2, 1, 1)
-    plt.plot(t, xvals, 'r-', label='$x$')
-    plt.ylabel('$x$')
+    plt.plot(t, xdot, 'r-', label='$\dot{x}$')
+    plt.title('Velocity as a Function of Time')
+    plt.ylabel('$\dot{x}$')
     plt.legend(loc='upper left')
 
-    ax2=plt.twinx()
-    ax2.plot(t, xdot, 'b-', label='$\dot{x}$')
-    plt.ylabel('$\dot{x}$')
-    plt.legend(loc='lower right')
-
-    plt.subplot(2,1,2)
+    # Plot p1 and p2 as functions of time
+    plt.subplot(2, 1, 2)
     plt.plot(t, p1, 'b-', label='$P_1$')
     plt.plot(t, p2, 'r-', label='$P_2$')
-    plt.legend(loc='lower right')
-    plt.xlabel('Time, s')
-    plt.ylabel('$P_1, P_2 (Pa)$')
+    plt.title('Pressures as a Function of Time')
+    plt.xlabel('Time (s)')
+    plt.ylabel('$P_1, P_2$ (Pa)')
+    plt.legend(loc='upper right')
 
+    # Show the plots
+    plt.tight_layout()
     plt.show()
+
 # endregion
 
 # region function calls
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
 # endregion
+
